3ee972cbaaadcfb742f28c9d23e384c1b4581f41
unknown <ananddey.nic@gmail.com>
2020-02-17 20:56:17 +0530

    complete login&registration

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,8 @@
 require_once 'config/config.php';
 //Load Helpers
 require_once 'helpers/url_helper.php';
+//SEssion Helper
+require_once 'helpers/session_helper.php';
 //Autoload Core Libraries
 spl_autoload_register(function($className){
 require_once 'libraries/'.$className.'.php';
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -59,6 +59,7 @@
 
                         // Register users
                         if($this->userModel->register($data)) {
+                           flash('register_success','You are registered and can log in');
                             redirect('users/login');
                         } else {
                             die('Something went Wrong');
@@ -106,13 +107,28 @@
             //Validate Password
             if(empty($data['password'])) {
                 $data['password_err']='Please enter Password';
-            } elseif(strlen($data['password']) < 6) {
-                $data['password_err']='Password must be 6 character long';
+            }
+            
+            //Check for User/ Email
+            if($this->userModel->findUserbyEmail($data['email'])) {
+                //User Found
+
+            } else {
+                $data['email_err']='No user found';
             }
             //Make Sure errors are empty
             if(empty($data['email_err']) && empty($data['password_err'])) {
                 //Validated
-                die('Success');
+                //Check and logged in users
+                $loggedInUsers=$this->userModel->login($data['email'],$data['password']);
+
+                if($loggedInUsers){
+                    //Create Session
+                    $this->createUserSession($loggedInUsers);
+                } else {
+                    $data['password_err']='Password Incorrect';
+                    $this->view('users/login',$data);
+                }
             } else {
                 //Load view with errors
                 $this->view('users/login',$data);
@@ -131,6 +147,26 @@
                 ];
                 //Load view
                 $this->view('users/login',$data);
+            }
+        }
+        public function createUserSession($user) {
+            $_SESSION['user_id']=$user->id;
+            $_SESSION['user_email']=$user->email;
+            $_SESSION['user_name']=$user->name;
+            redirect('pages/index');
+        }
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('users/login');
+        }
+        public function isLoggedIn() {
+            if(issset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
             }
         }
     }
